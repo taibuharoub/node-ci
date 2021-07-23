ba96e2294b0fafdd82151cc32016f04e5b34bd20
Taibu Haroub <haroubtaibu@gmail.com>
2021-07-23 21:34:25 +0300

    Setup auth and feed routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
+const feedRoutes = require("./feed")
+const authRoutes = require("./auth");
+
 const server = (app) => {
     app.get("/", (req, res, next) => {
       res.status(200).json({ message: "Wiring Up Continuous Integration" });
     });
+    app.use("/feed", feedRoutes);
+    app.use("/auth", authRoutes);
   };
   
   module.exports = server;
